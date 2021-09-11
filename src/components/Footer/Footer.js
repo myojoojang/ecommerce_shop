ac1de98c741969502ed0e5dbe7be849bc8f568f2
myojoojang <mjjang0127@gmail.com>
2021-09-11 17:39:11 +0900

    change build script

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Footer } from './styles'
+const Footer = () => {
+  return (
+    <>
+      <Footer>
+      </Footer>
+    </>
+  );
+}
+
+export default Footer;
